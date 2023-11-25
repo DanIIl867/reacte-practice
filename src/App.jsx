cd53645d1774256ad88890577c6c8a1584614931
Daniil <negativdaniil4@gmail.com>
2023-11-25 13:29:40 +0200

    reacte

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+// export const App = () => {
+// 	return (
+// 		<div
+// 			style={{
+// 				height: '100vh',
+// 				display: 'flex',
+// 				justifyContent: 'center',
+// 				alignItems: 'center',
+// 				fontSize: 40,
+// 				color: '#010101'
+// 			}}
+// 		>
+// 			React homework template
+// 		</div>
+// 	);
+// };
+
+import './App.css';
+import { Painting } from './Painting'
+
+export function App() {
+	return (
+		<>
+			<Painting />
+		</>
+	)
+}
