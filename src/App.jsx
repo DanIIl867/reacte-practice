8ed2c21e386bc3c5330de04ad932e233c393dba3
Daniil <negativdaniil4@gmail.com>
2023-12-16 13:20:40 +0200

    reacte-practice

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,13 @@
 
 import './App.css';
 // import { Header } from './components/Header'
+import { colorPickerOptions } from './colorOptions'
 import painting from './painting.json'
 import { PaintingList } from './components/PaintingList/PaintingList'
+import styles from 'App.module.css'
+import { ColorPicker } from 'components/ColorPicker/ColorPicker';
+import { Alert } from 'components/Alert/Alert';
+const box = { color: 'red', fontSize: 18, margin: '50px 50px' };
 
 export function App() {
 	return (
@@ -29,6 +34,15 @@ export function App() {
 				</nav>
 			</Header> */}
 			<PaintingList painting={painting} />
+
+			{/* <div style={box}>бокс</div>
+			<p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ex totam non optio eligendi! Porro autem quae non nisi quam nam aut nesciunt, tempore fuga sequi totam mollitia eligendi nobis?</p>
+			<p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam ex totam non optio eligendi! Porro autem quae non nisi quam nam aut nesciunt, tempore fuga sequi totam mollitia eligendi nobis?</p> */}
+
+			<ColorPicker options={colorPickerOptions} />
+			<Alert text="Останнж попередження" type="warning" />
+			<Alert text="Ура! Все ок!" type="success" />
+			<Alert text="О жах - все пропало" type="error" />
 		</div>
 	)
 }
--- src/components/Painting/Painting.jsx
+++ src/components/Painting/Painting.jsx
@@ -1,14 +1,15 @@
+import s from './Painting.module.css'
 import PropTypes from 'prop-types';
 
 export const Painting = ({ title, url, quantity, price, author }) => {
 	return (
-		<li>
+		<li className={s.list}>
 			<img src={url} alt="" width="480" />
 			<h2>{title}</h2>
-			<p>Автор: <a href={author.url}>{author.url}</a>{author.tag}</p>
+			<p>Автор: <a className={s.link} href={author.url}>{author.url}</a>{author.tag}</p>
 			<p>Ціна: {price} грн</p>
 			<p>Доступність: {quantity < 5 ? 'закінчується' : `є в наявності ${quantity}`}</p>
-			<button type="button">Додати в кошик</button>
+			<button className={s.btn} type="button">Додати в кошик</button>
 		</li>
 	)
 }
